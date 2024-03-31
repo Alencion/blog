c98b436f4d22f3c1bf98beb1c83df83310e7e35c
Alencion <rhqnrdl625@gmail.com>
2024-03-31 10:56:43 +0900

    init

---
+++ adaptor/in/web-server/src/main/java/com/alencion/blog/adaptor/in/WebServerApplication.java
@@ -0,0 +1,9 @@
+package com.alencion.blog.adaptor.in;
+
+import org.springframework.boot.SpringApplication;
+
+public class WebServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebServerApplication.class, args);
+    }
+}
